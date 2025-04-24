622a30aecd06209fae368d23aa7042190fabdaa8
bkrrajmali <bkrrajmali@gmail.com>
2025-04-24 02:47:41 +0000

    Updated versions.tf

--- versions.tf
+++ versions.tf
@@ -10,13 +10,3 @@ provider "azurerm" {
   features {}
   subscription_id = "202d4be6-e0dd-4b9e-84b7-e235d53271a8"
 }
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "NEWRG"
-    storage_account_name = "azurekssvc"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-  }
-}
-
