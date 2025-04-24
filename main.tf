36b92edbbd26260de770e0eb3faf173ba1bd8af2
bkrrajmali <bkrrajmali@gmail.com>
2025-04-24 01:54:53 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "resource_group" {
-  source              = "./modules/resource_group"
+  source = "git::https://shaurya123@bitbucket.org/azure-aks-modules/modules//resource_group?ref=main"
   resource_group_name = var.resource_group_name
   location            = var.location
   tags                = var.tags
 }
 
 module "acr" {
-  source              = "./modules/acr"
+  source              = "git::https://shaurya123@bitbucket.org/azure-aks-modules/modules//acr?ref=main"
   name                = var.acr_name
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
@@ -18,7 +18,7 @@ module "acr" {
 
 
 module "network" {
-  source                          = "./modules/network"
+ source = "git::https://shaurya123@bitbucket.org/azure-aks-modules/modules//network?ref=main"
   vnet_name                       = var.vnet_name
   vnet_address_space              = var.vnet_address_space
   aks_subnet_name                 = var.aks_subnet_name
@@ -35,7 +35,7 @@ module "network" {
 module "aks" {
   system_node_pool    = var.system_node_pool
   user_node_pool      = var.user_node_pool
-  source              = "./modules/aks"
+  source = "git::https://shaurya123@bitbucket.org/azure-aks-modules/modules//aks?ref=main"
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
   cluster_name        = var.cluster_name
