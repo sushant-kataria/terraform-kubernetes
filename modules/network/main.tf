a714403690a5fd285eabb27debec05a9c4737168
Sushant Kataria <skataria21@outlook.com>
2025-04-30 23:39:25 +0000

    main.tf created online with Bitbucket

---
+++ modules/network/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = var.aks_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.aks_subnet_address_prefix
+}
+
+resource "azurerm_subnet" "endpoints" {
+  name                 = var.endpoints_subnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.endpoints_subnet_address_prefix
+}
